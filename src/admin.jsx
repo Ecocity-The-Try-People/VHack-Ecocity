1c0071601ec89b5eea7fb66d35302e8c8eff9382
EvinKor <evin8917@gmail.com>
2025-03-21 20:55:48 +0800

    fixed_css

---
+++ src/admin.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import PublicAdminModule from '@/pages/PublicAdmin'
+
+function admin() {
+
+  return (
+    <>
+      <div>
+        <PublicAdminModule />
+      </div>
+    </>
+  )
+}
+
+export default admin
