b684b15facfdeefebc03082ebf0f098377ffe9df
EvinKor <evin8917@gmail.com>
2025-03-21 23:59:08 +0800

    merge_jiayin_page

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,22 @@ import Homepage from './page/homepage';
 import Flood_page from './page/flood_page';
 import Traffic_page from './page/traffic_page';
 import Smart_waste_management_page from './page/smart_waste_management_page';
-import Admin from './admin';
 import PublicAdminModule from '@/pages/PublicAdmin'
 
 function App() {
 
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Login />} />
-    //     <Route path="/register" element={<Register />} />
-    //     <Route path="/homepage" element={<Homepage />} />
-    //     <Route path="/traffic" element={<Traffic_page />} />
-    //     <Route path="/flood_page" element={<Flood_page />} />
-    //     <Route path="/smart_waste_management_page" element={<Smart_waste_management_page />} />
-    //     <Route path="/admin_page" element={<Admin />} />
-    //   </Routes>
-    // </Router>
-    <>
-    <div>
-      <PublicAdminModule />
-    </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/traffic" element={<Traffic_page />} />
+        <Route path="/flood_page" element={<Flood_page />} />
+        <Route path="/smart_waste_management_page" element={<Smart_waste_management_page />} />
+        <Route path="/admin_page" element={<PublicAdminModule />} />
+      </Routes>
+    </Router>
   );
 
 }
