f470d98ccfd47f1c5e914e47686ba4323f1d7ddd
Bryan2517 <bryanlowzy25@gmail.com>
2025-03-24 02:41:11 +0800

    Update PublicAdmin.jsx

--- src/page/PublicAdmin.jsx
+++ src/page/PublicAdmin.jsx
@@ -38,7 +38,7 @@ export default function Dashboard() {
         muted
         className="fixed top-0 left-0 w-full h-full object-cover z-0"
       >
-        <source src="src/assets/Smart-City.mp4" type="video/mp4" />
+        <source src="src/assets/videos/Smart-City.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
