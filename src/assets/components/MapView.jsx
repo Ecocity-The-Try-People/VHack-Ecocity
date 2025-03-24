a89d4e057412d2c5b5776ca8f5f9af073020d8de
Bryan2517 <bryanlowzy25@gmail.com>
2025-03-25 07:26:49 +0800

    function page done

--- src/assets/components/MapView.jsx
+++ src/assets/components/MapView.jsx
@@ -1,14 +1,58 @@
 import { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+// Recenter button component matching VehicleMap.jsx style
+const RecenterButton = ({ position }) => {
+  const map = useMap();
+  
+  const handleClick = () => {
+    map.flyTo(position, map.getZoom(), {
+      duration: 0.25,
+      easeLinearity: 0.25
+    });
+  };
+
+  return (
+    <button
+      className="recenter-button bg-gray-800 text-white p-2 rounded-full shadow-lg hover:bg-gray-700 transition-colors"
+      onClick={handleClick}
+      style={{
+        position: 'absolute',
+        bottom: '20px',
+        right: '20px',
+        zIndex: 1000
+      }}
+      aria-label="Recenter to my location"
+    >
+      <i className="fa-solid fa-location-crosshairs"></i>
+    </button>
+  );
+};
+
 const MapView = () => {
-  // 
   const [userPosition, setUserPosition] = useState(null);
-  // 
   const [garbageTruckPosition, setGarbageTruckPosition] = useState([3.139, 101.686]);
 
-  // 
+  // Red icon for user location
+  const redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  // Truck icon for garbage truck
+  const truckIcon = new L.Icon({
+    iconUrl: 'src/assets/png/garbage.png',
+    iconSize: [40, 40],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+  });
+
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -17,15 +61,14 @@ const MapView = () => {
           setUserPosition([latitude, longitude]);
         },
         (error) => {
-          console.error("error catching location:", error);
+          console.error("Error getting location:", error);
         }
       );
     } else {
-      console.error("Error");
+      console.error("Geolocation is not supported by this browser.");
     }
   }, []);
 
-  
   useEffect(() => {
     const interval = setInterval(() => {
       setGarbageTruckPosition((prev) => [
@@ -38,26 +81,31 @@ const MapView = () => {
   }, []);
 
   return (
-    <MapContainer 
-      center={userPosition || garbageTruckPosition}
-      zoom={13} 
-      style={{ height: "400px", width: "100%" }}
-    >
-   
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <div style={{ position: 'relative', height: '400px', width: '100%' }}>
+      <MapContainer 
+        center={userPosition || garbageTruckPosition}
+        zoom={13} 
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer 
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
 
-   
-      {userPosition && (
-        <Marker position={userPosition}>
-          <Popup> Your Location</Popup>
+        {userPosition && (
+          <>
+            <Marker position={userPosition} icon={redIcon}>
+              <Popup>Your Location</Popup>
+            </Marker>
+            <RecenterButton position={userPosition} />
+          </>
+        )}
+
+        <Marker position={garbageTruckPosition} icon={truckIcon}>
+          <Popup>Garbage Truck Location</Popup>
         </Marker>
-      )}
-
-     
-      <Marker position={garbageTruckPosition}>
-        <Popup> Garbage car location</Popup>
-      </Marker>
-    </MapContainer>
+      </MapContainer>
+    </div>
   );
 };
 
