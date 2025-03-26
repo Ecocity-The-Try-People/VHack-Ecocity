ca57ab0549c063792e72ffd35372445528ac5262
EvinKor <evin8917@gmail.com>
2025-03-26 20:47:53 +0800

    update

--- src/components/smart_waste_management/RequestPickup.jsx
+++ src/components/smart_waste_management/RequestPickup.jsx
@@ -204,7 +204,7 @@ const RequestPickup = () => {
                     <select
                         value={type}
                         onChange={(e) => setType(e.target.value)}
-                        className={`w-full border ${isDarkMode ? "bg-gray-800 text-white": "bg-white text-gray-700"} transition-all duration-300 border-gray-300 dark:border-gray-600 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:border-transparent`}
+                        className={`w-full border ${isDarkMode ? "bg-gray-700 text-gray-400 border-gray-600": "bg-white text-gray-500 border-gray-300"} transition-all duration-300 rounded-lg px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:border-transparent`}
                     >
                         <option value="Plastic">Plastic</option>
                         <option value="Paper">Paper</option>
